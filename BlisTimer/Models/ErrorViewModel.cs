f8b32c18c82951a7e035fcf9d5a984624899f31f
xToxette <x.tox@hotmail.com>
2022-10-04 09:04:11 +0200

    Add project files.

---
+++ BlisTimer/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace BlisTimer.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
