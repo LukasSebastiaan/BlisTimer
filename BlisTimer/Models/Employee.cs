6c87f0bb4a5bea8938c584cd5eb2b2f67f31c34c
LukasSebastiaan <wevers.luke@gmail.com>
2022-11-18 16:56:37 +0100

    add email and password to Employee.

--- BlisTimer/Models/Employee.cs
+++ BlisTimer/Models/Employee.cs
@@ -5,7 +5,9 @@ namespace BlisTimer.Models
         public string Id { get; set; } = null!;
         public string Name { get; set; } = null!;
         public string LastName { get; set; } = null!;
+        public string Email { get; set; } = null!;
         public int Role { get; set; }
+        public string Password { get; set; } = null!;
         public List<Project>? Projects { get; set; } = new List<Project>();
     }
 }
