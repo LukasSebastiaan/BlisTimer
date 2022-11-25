0ab863c8011d4145c704b33cf36b14b120146a4d
Darius van Essen <1026751@hr.nl>
2022-11-25 22:42:51 +0100

    boem

--- BlisTimer/Controllers/TimerController.cs
+++ BlisTimer/Controllers/TimerController.cs
@@ -1,40 +1,57 @@
 ﻿using BlisTimer.Data;
+using BlisTimer.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using Project = SimplicateAPI.Enitities.Project;
 
 namespace BlisTimer.Controllers
 {
     public class TimerController : Controller
     {
-        private readonly TimerDbContext _context;
-        private readonly ApiDatabaseHandler _apiDatabaseHandler;
-
-        public TimerController(TimerDbContext context, ApiDatabaseHandler apiDatabaseHandler)
+        private TimerDbContext _context { get; set; }
+        public TimerController(TimerDbContext context)
         {
             _context = context;
-            _apiDatabaseHandler = apiDatabaseHandler;
         }
-        
-        public async Task<IActionResult> Index(string id)
+        public IActionResult Index()
         {
-            await _apiDatabaseHandler.SyncDbWithSimplicate();
+            var projectId = HttpContext.Session.GetString("ProjectId");
+            var projectDict = new Dictionary<BlisTimer.Models.Project, List<WorkActivity>>();
+                
+            if (String.IsNullOrEmpty(projectId))
+            {
+                ViewBag.PreSelectedProject = true;
+                foreach (var project in _context.Projects.Include(_ => _.Activities))
+                {
+                    projectDict[project] = new List<WorkActivity>();
+                    foreach (var activity in project.Activities)
+                    {
+                        projectDict[project].Add(activity);
+                    }
+                }    
+            }
+            else
+            {
+                ViewBag.PreSelectedProject = false;
+                var p = _context.Projects.Include(_ => _.Activities).Where(_ => _.Id == projectId).FirstOrDefault();
+                projectDict[p] = new List<WorkActivity>();
+                foreach (var activity in p.Activities)
+                {
+                    projectDict[p].Add(activity);
+                    
+                }    
+            }
             
-            var activityId = HttpContext.Session.GetString("ActivityId");
-            if (!String.IsNullOrEmpty(id))
-            {
-                HttpContext.Session.SetString("ActivityId", id);
-                var activity = _context.WorkActivities.Include(_ => _.Project).Where(_ => _.Id == id).FirstOrDefault();
-                return View(activity);
-            }
-            else if (!String.IsNullOrEmpty(activityId))
-            {
-                id = activityId;
-                var activity = _context.WorkActivities.Include(_ => _.Project).Where(_ => _.Id == id).FirstOrDefault();
-                return View(activity);
-            }
-
-
-            return RedirectToAction("Index", "Projects");
+            
+            return View(new SelectProject(){ ProjectDictionary = projectDict});
+        }
+        [HttpPost]
+        public IActionResult Index(string Id)
+        {
+            
+            HttpContext.Session.SetString("ProjectId", Id);
+            
+            return RedirectToAction("Index");
         }
     }
-}
+ }
